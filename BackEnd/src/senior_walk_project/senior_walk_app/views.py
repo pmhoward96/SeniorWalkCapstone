f36638c673f71a9bd1b91add79af089913aa24fa
NicksComingUp <remember.marsman@gmail.com>
2019-02-02 15:47:33 -0600

    showing how to get values from the parameters

--- BackEnd/src/senior_walk_project/senior_walk_app/views.py
+++ BackEnd/src/senior_walk_project/senior_walk_app/views.py
@@ -3,7 +3,6 @@ from . import models
 from . import serializers
 
 from rest_framework import viewsets
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 # Create your views here.
