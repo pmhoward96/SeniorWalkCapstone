1a1cefc2faa5873e988089e5592748cc50ebe682
NicksComingUp <remember.marsman@gmail.com>
2019-02-02 14:33:30 -0600

    setting up the DATABASE FORMAT

--- BackEnd/src/senior_walk_project/senior_walk_app/models.py
+++ BackEnd/src/senior_walk_project/senior_walk_app/models.py
@@ -1,1 +1,11 @@
+from django.db import models
 
+# Create your models here.
+class Student(models.Model):
+  Fname = models.CharField(max_length=50)
+  Lname = models.CharField(max_length=50)
+  year  = models.IntegerField(default=0)
+  lat = models.DecimalField( max_digits=8, decimal_places=5, blank = True,  null = True)
+  long = models.DecimalField( max_digits=8, decimal_places=5, blank = True,  null = True)
+  # number = models.IntegerField(default=0)
+  # active = models.BooleanField(default=True)
