23b631b9e57dbf3e982f08d5ff7574c0909fd917
NicksComingUp <remember.marsman@gmail.com>
2019-02-02 15:41:35 -0600

    Create a view set class and and comment of Override default function in the rest frame work

--- BackEnd/src/senior_walk_project/senior_walk_app/views.py
+++ BackEnd/src/senior_walk_project/senior_walk_app/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from . import models
+from . import serializers
 
+from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
+
+class StudentViewSet(viewsets.ModelViewSet):
+    """provide a list of all Students back"""
+    queryset= models.Student.objects.all()
+    serializer_class = serializers.StudentSerializer
+    """RestFul API methods for the student data set"""
+    # def get_queryset(self):
+    #     """Overrides the GET methods"""
+    #     pass
+    # def create(self, request, *arg, **kwargs)
+    # """Override default POST"""
+    #     pass
+    # For debuging ---  import pdb; pdb.set_trace()
+    # def update(self, request, *arg, **kwargs)
+    # """Override default PUT"""
+    #     pass
+    # def partial_update(self, request, *arg, **kwargs)
+    # """Override default PATCH"""
+    #     pass
+    # def destroy(self, request, *arg, **kwargs)
+    # """Override default DELETE"""
+    #     pass
+
+
+    # Getting information from param in django method
+    # localhost:8001/student?id=23
+    # (self.request.query_params.get(param_name,default))
