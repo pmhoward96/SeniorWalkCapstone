312189f9bb9bfabe7312520730dd6d8fb38f84ee
Adam Ferrier <adamferrier479@gmail.com>
2019-02-03 13:58:12 -0600

    Added functionality to form elements
    Year is now a drop down menu from 1850 to 2016. Obviously we can change these. Name and year are stored to InputField's state and are displayed as an alert when submit is hit.

--- FrontEnd/src/InputFields.js
+++ FrontEnd/src/InputFields.js
@@ -1,34 +1,75 @@
 import React from 'react';
 
 export class InputFields extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: '',
+            year: 2016
+        };
+
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleYearChange = this.handleYearChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleNameChange(event) {
+        this.setState({name: event.target.value});
+    }
+
+    handleYearChange(event) {
+        this.setState({year: event.target.value});
+    }
+
+    handleSubmit(event) {
+      alert('selected: ' + this.state.name + ' and ' + this.state.year);
+      event.preventDefault(); // stops page from reloading after alert
+    }
+
+    handleReset(event) {
+        // do something
+    }
+
     render() {
+
+        // preparing list of dates for drop down select
+        let firstYear = 1850;
+        let lastYear = 2016;
+        let years = [];
+        for (let i = firstYear; i <= lastYear; i++) {
+            years.push(i);
+        }
+
+        const yearList = years.map((x) => {return(<option key={x}>{x}</option>)});
+
         return (
             <div>
                 <h1 className={"App-title"}>
                     Senior Walk
                 </h1>
-                < div className = "App-search" >
-                    <p>
-                        < input type = "text" />
-                        Name
-                    </p>
-                    <p>
-                        <input type="number"/>
-                        Year
-                    </p>
-                </div>
-                <div>
-                    <p>
-                        <button className="App-button">
-                            Submit
-                        </button>
-                    </p>
-                    <p>
-                        <button className="App-button">
-                            Reset Map
-                        </button>
-                    </p>
-                </div>
+
+                <form onSubmit={this.handleSubmit}
+                      onReset={this.handleReset}>
+                    <label>
+                        Name:
+                        <input type="text"
+                               value={this.state.name}
+                               onChange={this.handleNameChange} />
+                    </label>
+                    <br/>
+                    <label>
+                        Year:
+                        <select value={this.state.year}
+                                onChange={this.handleYearChange}>
+                            {yearList}
+                        </select>
+                    </label>
+                    <br/> <br/>
+                    <input type="submit" value="Submit" />
+                    <input type="reset" value="Reset" />
+                </form>
+
             </div>
         );
     }
